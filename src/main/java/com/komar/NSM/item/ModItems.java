04efd4e57a01f8acd4efab35fc3ce31a9fe8c5b1
komar <frimeice@gmail.com>
2026-08-19 21:28:55 +0300

    FREAKING STEEEEELLLLLLLLL

---
+++ src/main/java/com/komar/NSM/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.komar.NSM.item;
+
+import com.komar.NSM.Knsm;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Knsm.MODID);
+
+    public static final DeferredItem<Item> STEEL = ITEMS.register("steel",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
+
+}
